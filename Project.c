cc672a465ff99dcbd0355787f0b8e1a63ccc97bc
David Nathan <20356245@student.uwa.edu.au>
2012-09-18 13:52:39 +0800

    Didn't add the updated Project.c last commit

--- Project.c
+++ Project.c
@@ -23,22 +23,21 @@ void DieWithSystemMessage(const char *msg) {
 }
 
 int countLines(char* str_filename){
-     FILE *fd;
-     if((fd = fopen(str_filename, "r")) = NULL){
-              DieWithSystemMessage("Unable to open file");
-     }
+     FILE *fd = fopen(str_filename, "r");
      
-      unsigned int line_count = 0;
-  while ( (ch = fgetc(fd)) != EOF){
-     if (ch == EOL || ch == CARRIAGE_RETURN) ++line_count;
-         }
- 
-  if (fd) {
-     fclose(fd);
-  }
- 
-  return line_count;
-
+     if(fd == NULL){ DieWithSystemMessage("Unable to open file"); } //Test that file was opened
+              
+     unsigned int line_count = 0;
+     char ch;
+      
+     while ( (ch = fgetc(fd)) != EOF) //loop through each character in file. 
+         {
+      if (ch == EOL || ch == CARRIAGE_RETURN){ ++line_count;} //increase line_count if end-of-line or carriage-return is encountered
+          }
+         
+     if (fd) { fclose(fd);}
+     
+     return line_count;
       } 
 
 int main(int argc, char *argv[]){
@@ -50,6 +49,7 @@ int main(int argc, char *argv[]){
     char* sched = argv[1]; //Type of schedule 
     char* file = argv[2]; //Name of file that contains jobs
     int timeQuant;        //Time quantum for RR scheduling
+    int numJobs;          //Number of Jobs
     
     if(strcmp(sched, roundRobin) == 0){ 
        if(argc == 3){//Test for correct number of arguments for RR
@@ -58,21 +58,8 @@ int main(int argc, char *argv[]){
         timeQuant = atoi(argv[3]); //Set time quantum
     } 
     
-    FILE *infile = fopen(sched, "r");
-    if(infile == NULL){ //Test that file was opened
-    DieWithSystemMessage("Unable to open jobs file");
-    }
+   numJobs = countLines(file);
+   
+   printf("Number of Jobs: %d\n", numJobs);
     
-    
-    
-    
-    
-    
-
-       
-
-    
-    
-
-
 }
